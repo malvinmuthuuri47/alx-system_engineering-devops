fa761a13a4605c8adc0a81b98bedbb095e943ddc
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-03-04 07:37:16 +0300

    Make files executable

--- 0x06-regular_expressions/5-beginning_and_end.rb
+++ 0x06-regular_expressions/5-beginning_and_end.rb
@@ -1,3 +1,6 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches a string that starts with h ends with n and can have any single character in between
-puts ARGV[0].scan(/h.n/).join
+#a script matching a string that starts with h ends with
+#n and can have any single character in between
+
+puts ARGV[0].scan(/^h.n$/).join
+
--- 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -1,3 +1,4 @@
 #!/usr/bin/env ruby
-# A regular expression that is matches only capital letters
+#only matching: capital letters
+
 puts ARGV[0].scan(/[A-Z]/).join
