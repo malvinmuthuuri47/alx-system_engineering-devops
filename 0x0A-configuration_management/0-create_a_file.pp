44080899c4b3d5c565a21a1356eff38b44b16c18
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-04-26 01:20:35 +0300

    Task one

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,9 @@
+# This file will create a file using Puppet in /tmp
+
+file { '/tmp/school':
+    ensure  => file,
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => "I love Puppet\n",
+}
