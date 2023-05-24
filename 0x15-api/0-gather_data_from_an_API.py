ffda1e3f64ebfdffe589ad0d557101f0fedd60ca
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-24 18:50:18 +0300

    Task one correction

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
-"""Returns to-do list information for a given employee ID."""
+"""Accessing a REST API (jsonplaceholder) for todo lists of employees"""
+
 import requests
 import sys
 
-if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    name = requests.get("{}users/{}".format(url, sys.argv[1])).json()
-    tasks = requests.get("{}users/{}/todos".format(url, sys.argv[1])).json()
-    c_tasks = [task for task in tasks if task.get("completed") is True]
-    titles = [task.get("title") for task in tasks
-              if task.get("completed") is True]
+
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
+
+    response = requests.get(url)
+    employeeName = response.json().get('name')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+    done = 0
+    done_tasks = []
+
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
+
     print("Employee {} is done with tasks({}/{}):"
-          .format(name.get("name"), len(c_tasks), len(tasks)))
-    for title in titles:
-        print("\t {}".format(title))
+          .format(employeeName, done, len(tasks)))
+
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
