1e5aae8bb7bc9cc9afecb86c2ef6545378a436c4
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-05-24 15:18:03 +0300

    Task one

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "https://jsonplaceholder.typicode.com"
+    user_id = int(sys.argv[1])
+    user_endPoint = "{}/users/{}".format(url, user_id)
+    user_name = requests.get(user_endPoint).json().get("name")
+
+    tasks_endPoint = "{}/todos".format(url)
+    tasks = requests.get(tasks_endPoint).json()
+    user_tasks = [d for d in tasks if d.get("userId") == user_id]
+    tasks_completed = [d for d in user_tasks if d.get("completed")]
+    print("Employee {} is done with tasks({}/{}):"
+          .format(user_name, len(tasks_completed), len(user_tasks)))
+
+    for task in tasks_completed:
+        print("\t {}".format(task.get("title")))
