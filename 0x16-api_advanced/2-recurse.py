a921d454a496deabdc5aa7dd6377d4cb1fd0190b
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-06-08 16:02:40 +0300

    Fixed Documentation error

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,17 +1,14 @@
 #!/usr/bin/python3
-"""This modules queries the Reddit API
-"""
+"""This module queries the Reddit API"""
 import requests
 
 
 def recurse(subreddit, hot_list=[], after=None):
     """
     This function implements recursion where you query an API for a
-    given subreddit so long as the subreddit is valid, otherwise return
-    None
+    given subreddit so long as the subreddit is valid, otherwise return None
     """
-    url = "https://www.reddit.com/r/{}/hot.json?after={}"
-    .format(subreddit, after)
+    url = "https://www.reddit.com/r/{}/hot.json?after={}".format(subreddit, after)
     headers = {"User-Agent": "Edg/113.0.1774.57"}
 
     data = requests.get(url, headers=headers, allow_redirects=False)
@@ -23,5 +20,6 @@ def recurse(subreddit, hot_list=[], after=None):
 
         if after:
             recurse(subreddit, hot_list, after)
-        return (hot_list)
-    return (None)
+        return hot_list
+    return None
+
