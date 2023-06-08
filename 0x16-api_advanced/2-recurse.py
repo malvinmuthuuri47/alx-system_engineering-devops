83bd85ade099f95ff0b12d37d00c38d5dec6a71b
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-06-08 16:22:47 +0300

    Task 2 documentation error fix

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -8,7 +8,8 @@ def recurse(subreddit, hot_list=[], after=None):
     This function implements recursion where you query an API for a
     given subreddit so long as the subreddit is valid, otherwise return None
     """
-    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url = "https://www.reddit.com/r/{}/hot.json?after={}"
+    .format(subreddit, after)
 
     headers = {"User-Agent": "Edg/113.0.1774.57"}
 
