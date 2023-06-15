d6616a26eb3300c93942a8e91a64e8e93d616f58
malvinmuthuuri47 <malvinmuthuuri47@gmail.com>
2023-06-15 17:57:35 +0300

    Use exec for task 0

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,11 @@
-# This manifest repplaces the ULIMIT to accommodate concurrent requests
+#This puppet script increases the ulimit
+exec {'increase ulimit':
+  command => 'sed -i "s/15/2500/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+}
 
-file { '/etc/default/nginx':
-  ensure  => present,
-  content => 'ULIMIT="-n 2048"',
-  replace => true,
+# Restart nginx
+exec {'restart nginx':
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
